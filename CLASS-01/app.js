80d1424c1e7143a047486c2106771ec76584122a
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-02-24 15:55:34 +0500

    understanding connection TypeScript to JavaScript via terminal

---
+++ CLASS-01/app.js
@@ -0,0 +1,9 @@
+console.log("Hellow World");
+console.log("Hellow my dear Student");
+console.log("Hellow fahad");
+console.log("I have attend yesterday class");
+console.log("Sir taught us how to connect TypeScript file with JavaScript File");
+console.log("Sir also taught us installation of:");
+console.log("node.js");
+console.log("Visual Studio Code");
+console.log("TypeScript");
