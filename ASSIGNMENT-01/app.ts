ca7e4ce96054605490fc5f7cc0edd55cd770d38e
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-02-24 16:58:53 +0500

    updated

---
+++ ASSIGNMENT-01/app.ts
@@ -0,0 +1,59 @@
+                            // 45 Questions
+
+// Question 01
+
+// console.log("Node.js, TypeScript, VS Code -- have been installed");
+
+// Question 02
+
+var personName = "Hello Fahad";
+console.log(personName, "would you like to learn some python today")
+
+// Question 03
+
+var againName = "Hello Hassan";
+console.log(againName.toLowerCase());
+console.log(againName.toUpperCase());
+
+// Question 04
+
+var author = "Albert Einstein";
+var authorQuote = `${author} once said, "A person who never made a mistake never tried anything new" </br>`;
+console.log(authorQuote);
+
+// Question 05
+
+var famous_person = "Ghous Ahmed";
+var message = `${famous_person} once said, "A person who never made a mistake never tried anything new" </br>`;
+console.log(famous_person);
+
+// Question 06
+
+var whiteSpaceBwName = "\t Fahad Hassan \n";
+console.log("Name with white space");
+console.log("'" + whiteSpaceBwName + "'");
+var nameStripped = whiteSpaceBwName.trim();
+console.log("Name after Stripping White Space");
+console.log(nameStripped);
+
+// Question 07 & 08
+
+//  Subtraction 16 - 8 = 8
+
+var sub = 16 - 8;
+console.log(sub);
+
+//  Addition 4 + 4 = 8
+
+var add = 4 + 4;
+console.log(console.log(add));
+
+//  Multiplication 4 x 2 = 8
+
+var mul = 4 * 2;
+console.log(console.log(mul));
+
+//  Division 16 / 2 = 8
+
+var div = 16 / 2;
+console.log(console.log(div));
