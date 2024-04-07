1cad7dee3cff873c97937b6e1c7ceda08dc3979a
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-04-08 00:32:48 +0500

    completed

---
+++ CLASS-2/app.js
@@ -0,0 +1,24 @@
+// function sum(num1: number , num2: number){
+//     let add = num1 + num2
+//     console.log("sum of two numbers",add)
+//     return add
+// }
+// sum(5,5)
+// function sum1(num3: number,num4:number){
+//     let addition = num3 + num4
+//     console.log(addition)
+//     // return addition
+// }
+// sum1(2,2)
+function myName(name) {
+    console.log("Assalamualikum", name);
+}
+myName("Fahad");
+// function myName(name: any){
+//     if(typeof name === "string"){
+//         console.log("Hi",name)
+//     }else{
+//         console.error("invalid")
+//     }
+// }
+// myName(2)
