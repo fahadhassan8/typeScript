282833f08bf258cf38f51612f82ab5cb51265de0
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-05-21 00:43:47 +0500

    Create index.js

---
+++ INQUIRER-CLASS-05/index.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "Whats your name"
+    },
+    {
+        type: "number",
+        name: "age",
+        message: "Whats your age"
+    },
+    {
+        type: "list",
+        name: "gender",
+        message: "Whats your gender",
+        choices: ['Male', 'Female', "Prefer not to say"]
+    }
+]);
+console.log(`Your name is ${answer.name} Your age is ${answer.age} Your gender is ${answer.gender}`);
