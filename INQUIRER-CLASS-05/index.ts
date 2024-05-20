72cb645aa317413cd2dca3fec549468150cc1ce8
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-05-20 23:15:11 +0500

    Inquirer

---
+++ INQUIRER-CLASS-05/index.ts
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        type:'input',
+        name:'Name',
+        message:'What is your name'
+    },
+    {
+        type:'number',
+        age:'25',
+        message:'What is your age?'
+    },
+    {
+        type:'list',
+        name:'gender',
+        message:'What is your gender?',
+        choices:['Male','Female','Not to Say']
+    }
+])
+console.log(`Your Name is ${answer.name} Your Age is ${answer.age} Your Gender is ${answer.gender}`)
