099b8a8069cf997718b8e716b11ca6f5d88c7aea
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-05-30 00:23:40 +0500

    update

---
+++ Literal-Type/app.js
@@ -0,0 +1,14 @@
+// Literal Types and union
+// Syntex
+// let literalType : 'fahad' | 123 | [1,'a'] | true | {}
+//  assigned particularly fixed value not changable
+// let unionType : string | number | [] | {} | false
+// we can give any name number array value etc
+// Either string or number
+var literalType;
+literalType = 123;
+console.log(literalType);
+// Either string or number
+var unionType;
+unionType = 'fahad';
+console.log(unionType);
