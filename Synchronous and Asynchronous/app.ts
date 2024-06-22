dfa0af0202392a82d454038e85126e35525a2763
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-06-22 15:20:54 +0500

    updated

---
+++ Synchronous and Asynchronous/app.ts
@@ -0,0 +1,25 @@
+// Synchronous method
+// Line by Line
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+
+// Asynchronous method
+// console.log(1)
+// console.log(2)
+// setTimeout(function(){
+//     console.log(3)
+// },3000)
+// console.log(4)
+// console.log(5)
+
+// console.log(1)
+// console.log(2)
+
+// setTimeout(() => {
+//     console.log(3)
+// }, 1000);
+// for(let i = 0; i <= 5; i++){
+//     console.log(`Hello ${i}`)
+// }
